54e655c7a93d4fa058f0b2d45906dbaebb94ff83
Valentin Geringer <valentin.geringer@gymerg.de>
2026-09-14 14:37:54 +0000

    added logic for [file] [amount] and [start]

--- command-code/find-name.c
+++ command-code/find-name.c
@@ -1,6 +1,6 @@
 #include "../helpers.h"
 
-error_message find_name(char* options[], char* flags[])
+error_message find_name(char* options[], int opc, char* flags[], int flc)
 {
     error_message result;
     result.error = 0;
